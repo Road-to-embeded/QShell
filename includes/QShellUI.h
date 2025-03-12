0549432f8c722aa0e08cec1d83d0d03410315c3d
imxande <alerodriguezlarosa@gmail.com>
2025-03-11 21:25:35 -0400

    Add prompt to main window

--- includes/QShellUI.h
+++ includes/QShellUI.h
@@ -2,12 +2,19 @@
 #define QSHELL_UI_H
 
 #include <QMainWindow>
+#include <QPlainTextEdit>
 
 class QShellUI : public QMainWindow {
   Q_OBJECT // required
 
 public: 
   explicit QShellUI(QWidget *parent = nullptr);
+  ~QShellUI();
+
+  void displayShellPrompt(); // method to show prompt
+
+private:
+  QPlainTextEdit *editor; // terminal text area 
 
 };
 
--- src/QShellUI.cpp
+++ src/QShellUI.cpp
@@ -4,4 +4,21 @@ QShellUI::QShellUI(QWidget *parent) : QMainWindow(parent) {
   // WINDOW setup
   setWindowTitle("Main Window");
   resize(800, 600);
+
+  // Config new shell text area
+  editor = new QPlainTextEdit(this);
+  editor->setReadOnly(false);
+  setCentralWidget(editor);
+
+  // Show prompt
+  displayShellPrompt();
+}
+
+// Destructor
+QShellUI::~QShellUI(){};
+
+// Display prompt method
+void QShellUI::displayShellPrompt() {
+  // Add shell prompt
+  editor->appendPlainText("xande@Zero-Quantity:~$ ");
 }
